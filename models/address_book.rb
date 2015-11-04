7760af4cd4fae5861cf5c29441d77a8836010634
Ryan Kliemann <Ryan_blake@icloud.com>
2015-11-04 15:49:19 -0800

    check_22_assmt

--- models/address_book.rb
+++ models/address_book.rb
@@ -2,55 +2,82 @@ require_relative 'entry'
 require "csv"
 
 class AddressBook
- attr_accessor :entries
+  attr_accessor :entries
 
- def initialize
-   @entries = []
-end
+  def initialize
+    @entries = []
+  end
 
-# Search AddressBook for a specific entry by name
-def binary_search(name)
+  # Search AddressBook for a specific entry by name
+  def binary_search(name)
     return nil
   end
 
 
-def remove_entry(name, phone_number, email)
-   deleted_entry = nil
+  def remove_entry(name, phone_number, email)
+    deleted_entry = nil
 
-   @entries.each do |entry|
-     if name == entry.name && phone_number == entry.phone_number && email == entry.email
-       deleted_entry = entry
-     end
-   end
-   @entries.delete(deleted_entry)
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        deleted_entry = entry
+      end
+    end
+    @entries.delete(deleted_entry)
 
-end
-
-
- def add_entry(name, phone_number, email)
-
-   index = 0
-   @entries.each do |entry|
-
-   if name < entry.name
-     break
-   end
-   index += 1
   end
 
-  @entries.insert(index, Entry.new(name, phone_number, email))
- end
+
+  def add_entry(name, phone_number, email)
+
+    index = 0
+    @entries.each do |entry|
+
+      if name < entry.name
+        break
+      end
+      index += 1
+    end
+
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
 
 
-def import_from_csv(file_name)
-   csv_text = File.read(file_name)
-   csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
 
-   csv.each do |row|
-     row_hash = row.to_hash
-     add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
- end
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
 
 
- end
+  def binary_search(name)
+                        # The utility of this function is good for n names??
+# #1
+      lower = 0
+      upper = entries.length - 1
+
+# #2
+    while lower <= upper
+# #3
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+# #4
+    if name == mid_name
+      return entries[mid]
+    elsif name < mid_name
+      upper = mid - 1
+    elsif name > mid_name
+      lower = mid + 1
+    end
+    end
+
+# #5
+  return nil
+  end
+
+  
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../models/address_book'
+require_relative '../models/entry'
 
 RSpec.describe AddressBook do
   # #1
@@ -31,6 +32,7 @@ RSpec.describe AddressBook do
     end
   end
 
+
   describe "#binary_search" do
     it "searches AddressBook for a non-existent entry" do
       book = AddressBook.new
@@ -38,7 +40,49 @@ RSpec.describe AddressBook do
       entry = book.binary_search("Dan")
       expect(entry).to be_nil
     end
+
+
+  it "searches AddressBook for Bill" do
+    book.import_from_csv("entries.csv")
+    entry = book.binary_search("Bill")
+    expect(entry).to be_a Entry
+    check_entry(entry, "Bill", "555-555-4854", "bill@blocmail.com")
   end
+
+  it "searches AddressBook for Bob" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Bob")
+      expect(entry).to be_a Entry
+      check_entry(entry, "Bob", "555-555-5415", "bob@blocmail.com")
+  end
+
+    it "searches AddressBook for Joe" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Joe")
+      expect(entry).to be_a Entry
+      check_entry(entry, "Joe", "555-555-3660", "joe@blocmail.com")
+    end
+
+    it "searches AddressBook for Sally" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Sally")
+      expect(entry).to be_a Entry
+      check_entry(entry, "Sally", "555-555-4646", "sally@blocmail.com")
+    end
+
+    it "searches AddressBook for Sussie" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Sussie")
+      expect(entry).to be_a Entry
+      check_entry(entry, "Sussie", "555-555-2036", "sussie@blocmail.com")
+    end
+
+    it "searches AddressBook for Billy" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Billy")
+      expect(entry).to be_nil
+    end
+end
 
   describe "#remove_entry" do
     it "removes any entry by name, phone_number, email" do
@@ -187,4 +231,5 @@ RSpec.describe AddressBook do
       expect(entry_three.email).to eql "jay@mail.com"
     end
   end
+
 end
